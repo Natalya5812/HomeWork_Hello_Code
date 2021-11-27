966545bc3b94306f67de2a9b53d8be23cdb73712
Natalya5812 <Natgu87@mail.ru>
2021-11-27 19:34:40 +0400

    Dayofweek_array

--- 003_1/Program.cs
+++ 003_1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("Введите номер дня недели: ");
+int day = int.Parse (Console.ReadLine());
+string[] array = 
+{
+    "Неделя начинается с понедельника",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье"
+};
+
+if (day < 7 || day >=0)
+{
+    Console.WriteLine(array [day]);
+}
+
+else if (day > 7 || day <0)
+{
+    Console.WriteLine("В неделе 7 дней!");
+}
