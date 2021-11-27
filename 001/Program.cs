186d354ad05ba0a162e1004a0e98de2c574e2335
Natalya5812 <Natgu87@mail.ru>
2021-11-27 15:31:21 +0400

    001 x^2

---
+++ 001/Program.cs
@@ -0,0 +1,5 @@
+﻿// Вывести квадрат числа
+
+Console.Write("Введите число, которое необходимо возвести в квадрат: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(a + " в квадрате = " + a * a);
