d1e2f467a13ca4d61765ae14db2370541a1f9218
Natalya5812 <Natgu87@mail.ru>
2021-11-27 18:18:40 +0400

    MAX FROM 3

---
+++ 005/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти максимальное из трех чисел
+Console.Write ("Введите первое число: ");
+Double A = Convert.ToDouble (Console.ReadLine ());
+Console.Write ("Введите второе число: ");
+Double B = Convert.ToDouble (Console.ReadLine ());
+Console.Write ("Введите третье число: ");
+Double C = Convert.ToDouble (Console.ReadLine ());
+Double MAX = A;
+
+if (MAX < A)
+{
+Console.WriteLine ("MAX = " + A);
+}
+else if (MAX < B)
+{
+Console.WriteLine ("MAX = " + B);
+}
+else if (MAX < C)
+{
+Console.WriteLine ("MAX = " + C);
+}
+
