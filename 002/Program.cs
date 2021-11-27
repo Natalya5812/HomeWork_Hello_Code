c46604dc591cb28ac11f1a02f7a68f958f730144
Natalya5812 <Natgu87@mail.ru>
2021-11-27 16:18:07 +0400

    002 b*b=a

---
+++ 002/Program.cs
@@ -0,0 +1,14 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+if (b * b == a)
+{
+Console.WriteLine (a + " является квадратом " + b);
+}
+else 
+{
+Console.WriteLine (a + " не является квадратом " + b);
+}
