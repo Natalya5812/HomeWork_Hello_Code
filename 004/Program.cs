312652392248397686fe24b2fdfe20143ebbe4fa
Natalya5812 <Natgu87@mail.ru>
2021-11-27 17:41:13 +0400

    004 MIN-MAX

---
+++ 004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.Write ("Введите первое число: ");
+double A = Convert.ToDouble (Console.ReadLine ());
+Console.Write ("Введите второе число: ");
+double B = Convert.ToDouble (Console.ReadLine ());
+
+if (A > B)
+{
+    Console.WriteLine (A + " = MAX; MIN = " + B);
+}
+else
+{
+    Console.WriteLine (B + " = MAX; MIN = " + A);
+}
