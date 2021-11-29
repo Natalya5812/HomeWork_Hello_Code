8b71fce51228b378a742220711c9c04629e9d3f9
Natalya5812 <Natgu87@mail.ru>
2021-11-29 20:41:07 +0400

    006/ y=f(a)

---
+++ 006/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+// y = a-1, a>=0;
+// y = |a|, a<0
+
+Console.Write("Введите значение 'a': ");
+int a = int.Parse(Console.ReadLine());
+
+if(a >= 0)
+{
+    Console.WriteLine("y = " + (a - 1));
+}
+else
+{
+    Console.WriteLine("y = " + Math.Abs(a));
+}
