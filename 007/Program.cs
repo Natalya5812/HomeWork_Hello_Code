c7d666f2ea2269934b586fede661cee428f5c89e
Natalya5812 <Natgu87@mail.ru>
2021-11-29 21:01:27 +0400

    007 A%2

---
+++ 007/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+Console.Write ("Введите целое число: ");
+int A = int.Parse (Console.ReadLine ());
+
+if (A % 2 == 0)
+{
+    Console.WriteLine (A + " является четным числом");
+
+}
+else
+{
+    Console.WriteLine (A + " не является четным числом");
+}
